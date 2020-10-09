03d6f8aaa5d387c0249c5bccf7dda93f56c35395
anikkhann <anikkhann46@gmail.com>
2020-10-09 06:36:07 +0600

    done

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCdJSWWAJLJ2bmfJH2zqg2kM0x1ZlLac0M",
+  authDomain: "fire-auth-c727a.firebaseapp.com",
+  databaseURL: "https://fire-auth-c727a.firebaseio.com",
+  projectId: "fire-auth-c727a",
+  storageBucket: "fire-auth-c727a.appspot.com",
+  messagingSenderId: "773384757420",
+  appId: "1:773384757420:web:135781b1f5ffbb26c13f09"
+};
+export default firebaseConfig;
